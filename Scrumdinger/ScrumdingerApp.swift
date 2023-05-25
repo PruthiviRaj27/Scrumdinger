b55db8bbba198a855ec916cc762a46bc751b69bb
Prithuvi <prithuvi@Prithuvis-MacBook-Pro.local>
2023-05-25 13:55:23 +0530

    App initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Prithuvi on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
